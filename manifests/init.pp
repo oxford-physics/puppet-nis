731a4795f92379541e919b425ebf0709c49fed8a
Sean Brisbane <s.brisbane1@physics.ox.ac.uk>
2014-11-03 15:20:46 +0000

    Changes to incorporate x2 auth physics systems

--- manifests/init.pp
+++ manifests/init.pp
@@ -17,8 +17,6 @@
 #Limitations - only one or two extra auth parameters - cant do array iteration and index storing
 class nis::client ( 
    $nis_domain = $nis::params::nis_domain, 
-   $extraauth = $nis::params::extraauth, 
-   $extraauth2 = $nis::params::extraauth2,
    $servers  = $nis::params::servers,
    $ypbindport = $nis::params::ypbindport,
    ) inherits nis::params {
@@ -56,42 +54,7 @@ class nis::client (
                 require => Package["nis"],
                notify => Service["nis"],
         }
-  $extraauthint = "nis"
-       if $extraauth2 == "none"  {
-           $changeset=[
-            "set *[. = 'passwd']/service[1] files",
-            "set *[. = 'passwd']/service[2] $extraauth",
-            "set *[. = 'group']/service[1] files",
-            "set *[. = 'group']/service[2] $extraauth",
-            "set *[. = 'shadow']/service[1] files",
-            "set *[. = 'shadow']/service[2] $extraauth",
-             ]
-      }
-      else  { 
-          $changeset=[
-            "set *[. = 'passwd']/service[1] files",
-            "set *[. = 'passwd']/service[2] $extraauth",
-            "set *[. = 'passwd']/service[3] $extraauth2",
-            "set *[. = 'group']/service[1] files",
-            "set *[. = 'group']/service[2] $extraauth",
-            "set *[. = 'group']/service[3] $extraauth2",
-            "set *[. = 'shadow']/service[1] files",
-            "set *[. = 'shadow']/service[2] $extraauth",
-            "set *[. = 'shadow']/service[3] $extraauth2",
-             ]
-
-
-         }
-         
-    augeas{"nis":
-
-       context   => '/files/etc/nsswitch.conf/',
-       #This part changes options on an already existing line
-       #This only works if the nsswitch.conf has passwd, group and shadow entries.  So you wont get the file created.
-       changes   => $changeset,
-       notify    => Service['nis']
-     }
 
    #TODO - copy in nsswitch if it doesnt exist
-
+   notify {"Warning: Nis client module does not manage rpcbind":}
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -3,8 +3,6 @@ class nis::params (
           $nis_domain = hiera("nis::params::nis_domain", "npldecs"),
           #files is always first
           #pass extraauth
-          $extraauth = hiera("nis::params::extraauth",  "nis" ),
-          $extraauth2 = hiera("nis::params::extraauth2",  "none" ),
           $servers = hiera_array("nis::params::servers", ['ppypman'] ),
           #ypbind listens on a specific port.  We want to use iptables,
           $ypbindport = hiera("nis::params::ypbindport", 883 ),
